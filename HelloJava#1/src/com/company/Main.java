91693b70cb2271dce25fb2cff9882a99b5bd4d4f
scalesrzn <scalesrzn@gmail.com>
2020-12-19 06:05:07 +0300

    Поигрался с условными операторами, циклами и массивами

--- HelloJava#1/src/com/company/Main.java
+++ HelloJava#1/src/com/company/Main.java
@@ -26,13 +26,48 @@ public class Main {
 //---------------------------------------------
 //Поработаем с условными операторами
 //____________________________________________
-        Scanner sChar = new Scanner(System.in);
-        String sChVar;
-        System.out.print("Введите текст, а я проверю: ");
-        sChVar = sChar.next();
-        if (sChVar.equals("Hello"))
-            System.out.print("World");
-        else
-            System.out.print("Вы ввели :"+sChVar);
+//        Scanner sChar = new Scanner(System.in);
+//        String sChVar;
+//        System.out.print("Введите текст, а я проверю: ");
+//        sChVar = sChar.next();
+//        if (sChVar.equals("Hello"))
+//            System.out.print("World");
+//        else
+//            System.out.print("Вы ввели :"+sChVar);
+//-----------------------------------------------
+//        Scanner enter = new Scanner(System.in);
+//        int iInt = enter.nextInt();
+//        switch (iInt) {
+//            case 1:
+//                System.out.print("Вы ввели 1");
+//                break;
+//            case 2:
+//                System.out.print("Вы ввели 2");
+//                break;
+//            default:
+//                System.out.print("Вы ввели хуй пойми че");
+//        }
+//_________________________________________________________
+//Захуйрим пару циклов, ага
+//    for (int i = 0; i<20; i++){
+//        System.out.print(i);
+//    }
+//
+//    int j = 0;
+//    while(j<40){
+//        System.out.println(j);
+//        j+=10;
+//    }
+//
+//    int x = 0;
+//    do {
+//        x++;
+//        System.out.println(x);
+//    }
+//    while (x<1);
+//Ебурим массивы
+    int[] name;
+    name = new int[20];
+    name = new int[]{1,2,3,4,4,5,6,6,6,6,6,6,6,6,6,6,6,6};
     }
 }
